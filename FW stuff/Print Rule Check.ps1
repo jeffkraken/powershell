089db25eae6d5cbe7eaced2827ee7df99208a30a
abandonfreewifi <153031981+jeffkraken@users.noreply.github.com>
2024-10-29 18:12:58 -0500

    Add files via upload

---
+++ FW stuff/Print Rule Check.ps1
@@ -0,0 +1,13 @@
+﻿$rules = Get-NetFirewallRule | Where-Object {$_.Name -match "Spool|Print|Spooler|Printer"}
+
+if ($rules){
+    foreach ($rule in $rules){
+        if ($rule.Enabled -eq "True"){
+            $action = if($rule.Action -eq "Allow") {"allowed"} else {"blocked"}
+            Write-Host "$($rule.DisplayName) is enabled and is $action on $($rule.Program)"
+        }
+    }
+}
+else {
+Write-Host "No Print Rules Found."
+}
---
+++ FW stuff/pingFWrules.ps1
@@ -0,0 +1,23 @@
+﻿#Check if inbound ping rule is enabled.
+#Enable if not enabled.
+Get-NetFirewallRule | Where-Object {$_.DisplayName -like "File*ICMPv4*In*"}
+if ($_.Enabled -like "F*")
+{Enable-NetFirewallRule -DisplayName $_.DisplayName}
+else {$in = "T"}
+
+#Check if outbound ping rule is enabled.
+#enable if not enabled.
+Get-NetFirewallRule | Where-Object {$_.DisplayName -like "File*ICMPv4*Out*"}
+if ($_.Enabled -like "F*")
+{Enable-NetFirewallRule -DisplayName $_.DisplayName}
+else {$out = "T"}
+
+clear-host
+
+if ($in -like "T" -and $out -like "T")
+{Write-Host "Both inbound outbound ping rules were already enabled."}
+elseif ($in -notlike "T" -and $out -like "T")
+{Write-Host "Inbound rule was not set, it has now been set. `n Outbound rule was already set."}
+elseif ($in -like "T" -and $out -notlike "T")
+{Write-Host "Outbound rule was not set, it has now been set. `n Inbound rule was already set."}
+else {Write-Host "Neither inbound or outbound ping rules were set, but now they are."}
