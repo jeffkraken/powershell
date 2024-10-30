3152d19d3287c87394df4bdc17365f57df0acb60
abandonfreewifi <153031981+jeffkraken@users.noreply.github.com>
2024-10-30 18:27:26 -0500

    add w11sysinfo
    basic SOC task for W11 systems: Get sysinfo (hostname, os version, user, network) and display.

---
+++ w11sysinfo.ps1
@@ -0,0 +1,15 @@
+﻿#sysinfo
+
+$hostname = hostname
+$os_version = (Get-WmiObject -Class Win32_OperatingSystem).Version
+$current_user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+$network_info = Get-NetIPAddress | Select-Object InterfaceAlias, IPAddress, AddressFamily
+
+#Display
+
+Write-Output "Computer name: $hostname"
+Write-Output "OS Version: $os_version"
+Write-Output "Current User: $current_user"
+Write-Output "Network information: "
+$network_info | ForEach-Object {Write-Output "`t $($_.InterfaceAlias): $($_.IPAddress)"}
