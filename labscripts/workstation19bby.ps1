4b339d43bf40faaf7eabaf6d83e5f6dc052e1481
abandonfreewifi <153031981+jeffkraken@users.noreply.github.com>
2025-04-08 17:56:38 -0500

    Update workstation19bby.ps1

--- labscripts/workstation19bby.ps1
+++ labscripts/workstation19bby.ps1
@@ -1,4 +1,4 @@
-#Verify that DC19BBY gave Workstation19BBY a valid IP Address via DHCP 
+#Verify that DC19BBY gave Station19BBY a valid IP Address via DHCP 
 # Get-NetIPAddress -AddressFamily IPv4 | Format-Table
 
 #Set the Firewall to allow inbound and outbound ping over IPv4
@@ -10,15 +10,15 @@
 #Create the Certs folder if it doesn't exist
 # New-Item -Path C:\Certs -ItemType Directory -Force
 
-#Generate a Digital Certificate for Workstation19BBY 
+#Generate a Digital Certificate for the current user on Station19BBY 
 # $cert = New-SelfSignedCertificate -Subject $env:USERNAME -CertStoreLocation Cert:\LocalMachine\My
 # Export-Certificate -Cert $cert -FilePath C:\Certs\Certificate.cer
 
 #Share the new Digital Certificate over the network and allow "Everyone - Read" permissions
 # New-SmbShare -Path C:\Certs -Name Certs -ReadAccess Everyone
 
-# Set the Hostname to Workstation19BBY
-#Rename-Computer -NewName Workstation19BBY -Force
+# Set the Hostname to Station19BBY
+#Rename-Computer -NewName Station19BBY -Force
 
 # Restart the VM
 #Restart-Computer -Force
