ae41a2353748608828e62b0162cb229e9bc23ac4
abandonfreewifi <153031981+jeffkraken@users.noreply.github.com>
2025-04-15 14:33:30 -0500

    Update Station19bby.ps1
    removed restart

--- labscripts/Station19bby.ps1
+++ labscripts/Station19bby.ps1
@@ -1,24 +1,21 @@
 #Set the Firewall to allow inbound and outbound ping over IPv4
-# Enable-NetFirewallRule -Name FPS-ICMP4-*
+Enable-NetFirewallRule -Name FPS-ICMP4-*
 
 #Set the Time Zone to "Central Standard Time"
-# Set-TimeZone -Id "Central Standard Time"
+Set-TimeZone -Id "Central Standard Time"
 
 #Create the Certs folder if it doesn't exist
-# New-Item -Path C:\Certs -ItemType Directory -Force
+New-Item -Path C:\Certs -ItemType Directory -Force
 
 #Generate a Digital Certificate for the current user on Station19BBY 
-# $cert = New-SelfSignedCertificate -Subject $env:USERNAME -CertStoreLocation Cert:\LocalMachine\My
-# Export-Certificate -Cert $cert -FilePath C:\Certs\Certificate.cer
+$cert = New-SelfSignedCertificate -Subject $env:USERNAME -CertStoreLocation Cert:\LocalMachine\My
+Export-Certificate -Cert $cert -FilePath C:\Certs\Certificate.cer
 
 #Share the new Digital Certificate over the network and allow "Everyone - Read" permissions
-# New-SmbShare -Path C:\Certs -Name Certs -ReadAccess Everyone
+New-SmbShare -Path C:\Certs -Name Certs -ReadAccess Everyone
 
 #Firewall rules to ensure lab connectivity
-# Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing"
+Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing"
 
-# Set the Hostname to Station19BBY
-#Rename-Computer -NewName Station19BBY -Force
-
-# Restart the VM
-#Restart-Computer -Force
+#Set the Hostname to Station19BBY
+Rename-Computer -NewName Station19BBY -Force
