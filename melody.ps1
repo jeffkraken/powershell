9b1ab528debc212bdc0a14e9ebf3cb3912ca81db
abandonfreewifi <153031981+jeffkraken@users.noreply.github.com>
2026-07-23 17:43:57 -0500

    Update melody.ps1

--- melody.ps1
+++ melody.ps1
@@ -1,40 +1,40 @@
 # PowerShell Console.Beep melody demo
 # Uses only [Console]::Beep(frequency, duration)
 
-function N($f, $d) {
+function Note($f, $d) {
     [Console]::Beep($f, $d)
 }
 
-function R($d) {
+function Rest($d) {
     Start-Sleep -Milliseconds $d
 }
 
 # Intro
-N 392 400   # G4
-N 523 400   # C5
-N 659 700   # E5
-R 100
+Note 392 400   # G4
+Note 523 400   # C5
+Note 659 700   # E5
+Rest 100
 
-N 587 300   # D5
-N 523 500   # C5
-N 440 700   # A4
-R 150
+Note 587 300   # D5
+Note 523 500   # C5
+Note 440 700   # A4
+Rest 150
 
 # Main phrase
-N 392 300
-N 440 300
-N 523 500
-N 659 700
-R 100
+Note 392 300
+Note 440 300
+Note 523 500
+Note 659 700
+Rest 100
 
-N 698 400   # F5
-N 659 400
-N 587 500
-N 523 800
-R 200
+Note 698 400   # F5
+Note 659 400
+Note 587 500
+Note 523 800
+Rest 200
 
 # Ending
-N 392 300
-N 523 300
-N 659 500
-N 784 1000  # G5
+Note 392 300
+Note 523 300
+Note 659 500
+Note 784 1000  # G5
